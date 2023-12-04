4d1c448706a44163d8884296f85721b24e15c8c0
Vheek <111293033+Vheek@users.noreply.github.com>
2023-12-04 18:33:27 +0100

    Thiscommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-function App() {
+const mapContainerStyle = {
+  height: '400px',
+  width: '100%',
+};
+
+const center = {
+  lat: 6.5244,
+  lng: 3.3792,
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h2" align="center" style={{ margin: '20px 0' }}>
+        Gym Locator
+      </Typography>
+      <LoadScript googleMapsApiKey="AIzaSyD_t72hDaYxMztvrW3udG2UjlO5Ya62gBE">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={12}
+        />
+      </LoadScript>
+    </Container>
   );
-}
+};
 
 export default App;
